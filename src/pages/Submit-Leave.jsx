21b37d12bd051b58f5309a970fd6a3eab4317aac
Williamc2b <150145033+Williamc2b@users.noreply.github.com>
2025-04-04 14:51:48 +0100

    added submit leave to database

--- src/pages/Submit-Leave.jsx
+++ src/pages/Submit-Leave.jsx
@@ -1,19 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { database } from "../firebase_setup/firebase.js";
+import { collection, addDoc,doc,getDoc} from "firebase/firestore";
 
 function SubmitLeave() {
     const navigate = useNavigate();
-
+    const [userData, setUserData] = useState(null);
     const [leaveType, setLeaveType] = useState('');
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
 
-    const handleSubmit = (e) => {
+    useEffect(() => {
+        const userData = localStorage.getItem("user");
+        if (userData) {
+            setUserData(JSON.parse(userData));
+        }
+    }, []);
+
+    const handleSubmit = async(e) => {
         e.preventDefault();
-        alert(`Leave Submitted: ${leaveType} from ${startDate} to ${endDate}`);
-        setLeaveType('');
-        setStartDate('');
-        setEndDate('');
+        // Validate the form inputs
+        if (!leaveType || !startDate || !endDate) {
+            alert('Please fill in all fields');
+            return;
+        }
+        if (new Date(startDate) > new Date(endDate)) {
+            alert('Start date cannot be after end date');
+            return;
+        }
+        const userID = userData.id; // Assuming EmployeeID is stored in localStorage
+        try {
+            console.log('User ID:', userID);
+            const LeaveRequests = doc(database, 'LeaveRequests', userID);
+            const leaveDoc = await getDoc(LeaveRequests);
+            if (leaveDoc.exists()) {
+                alert('Leave request already exists for this user!');
+                return;
+            }
+            // Create a new document with the userID as the document ID
+            await addDoc(collection(database, 'LeaveRequests'), {
+                userID: userID,
+                leaveType: leaveType,
+                startDate: startDate,
+                endDate: endDate,
+                status: 'Pending',
+            });
+            alert('Leave request submitted successfully!');
+            navigate('/consultant-dashboard'); // Redirect to dashboard after successful submission
+        }
+        catch (error) {
+            console.error('Error checking leave request:', error);
+            alert('Error checking leave request. Please try again later.');
+            return;
+        }
     };
 
     return (
@@ -57,7 +96,7 @@ function SubmitLeave() {
                 </div>
 
                 <br />
-                <button type="submit">Submit</button>
+                <button type="submit" onClick={handleSubmit}>Submit</button>
             </form>
 
             <div style={{marginTop: '20px'}}>
