becbb4fbbc78fa0fbc22f8e784b2fb9d215e34c1
yasiralzaid <ec23981@qmul.ac.uk>
2025-04-07 19:27:07 +0100

    Finished consultant dashboard styling

--- src/pages/consultant-dashboard.jsx
+++ src/pages/consultant-dashboard.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import { useNavigate} from "react-router-dom";
 import './styles/Consultant-Dashboard.css';
 import fdmLogo from './styles/images/FDMlogo.png';
+import calendar from './styles/images/calendar_icon.png';
+import notif from './styles/images/notif_icon.png';
+import log_out from './styles/images/log_out.png';
+import profile from './styles/images/profile_icon.png';
+import submit_expense from './styles/images/submit_expense_icon.png';
+import submit_leave from './styles/images/submit_leave_icon.png';
+
+
+
 
 
 function Dashboard() {
@@ -16,16 +25,19 @@ function Dashboard() {
       <div className="dashboard-container">
         <img src={fdmLogo} alt="FDM Logo" className="Logo" />
         <br></br>
-          <button onClick={() => navigate("/work-schedule")} className="buttons">📅 Work Schedule</button>
-          <button onClick={() => navigate("/submit-leave")} className="buttons">📝 Submit Leave</button>
-          <button onClick={() => navigate("/submit-expense")} className="buttons">💰 Submit Expense</button>
-          <button onClick={() => alert("Viewing Notifications")} className="buttons">🔔 Notifications</button>
-          <button onClick={() => navigate("/update-info")} className="buttons">👤 Profile</button>
-          <div className="Logout">
-              <button onClick={handleLogout} style={{backgroundColor: "red", color: "white"}}>
-                  🚪 Logout
-              </button>
+        <button onClick={() => navigate("/work-schedule")} className="buttons"><img src={calendar} className="icon"></img> <br/>Work Schedule</button>
+        <div className="row">
+          <button onClick={() => navigate("/submit-leave")} className="buttons"> <img src={submit_leave} className="icon"></img> <br/> Submit Leave</button> 
+          <button onClick={() => navigate("/submit-expense")} className="buttons"> <img src={submit_expense} className="icon"></img> <br/> Submit Expense</button>
           </div>
+          <div className="row">
+          <button onClick={() => alert("Viewing Notifications")} className="buttons"> <img src={notif} className="icon"></img> <br/> Notifications</button>
+          <button onClick={() => navigate("/update-info")} className="buttons"> <img src={profile} className="icon"></img> <br/> Profile</button>
+
+          </div>
+              <button onClick={handleLogout} className="Logout">
+              <img src={log_out} className="icon"></img> Logout
+              </button>
       </div>
   );
 }
